e4685ec2e2228c853df55fae8398a80fd468d9a5
bardha cocaj <fc52610@ubt-uni.net>
2022-11-10 20:11:44 +0100

    about section

--- index.php
+++ index.php
@@ -37,18 +37,107 @@
    <section class="home" id="home">
 
    <div class="image" data-aos="fade-right">
-      <img src="images/AvatarMaker.png" alt="">
-      <img src="images/AvatarMaker.png" alt="">
-      <img src="images/AvatarMaker.png" alt="">
+      <img src="images/me2.png" alt="">
+      <img src="images/me2.png" alt="">
+      <img src="images/me2.png" alt="">
    </div>
    <div class="content">
-      <h3 data-aos="fade-up">hiii, I'm Fatbardha</h3>
+      <h3 data-aos="fade-up">hiii, I'm Fatbardha 👋</h3>
       <span data-aos="fade-up">Web designer & developer</span>
       <p data-aos="fade-up">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis labore recusandae minima molestias aliquam nostrum.</p>
       <a data-aos="fade-up" href="#about" class="btn">about me</a>
    </div>
 
 </section>
+
+
+<section class="about" id="about">
+
+
+
+<div class="row-1">
+
+    <div class="image">
+        <img src="images/about1.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3> Here's a bit about me</h3>
+        <p>I am currently a third-year student at UBT University, majoring in computer science, specializing in Web programming. 
+         <br>I am able to work in a team environment as well as using personal initiative, adaptable to challenging situations,I have a creative mind, dedicated to continuous learning, always ready for new challenges.</br></p>
+        <div class="box-container">
+            <div class="box">
+                <p> <span> age : </span> 20 </p>
+                <p> <span> gender : </span> female </p>
+                <p> <span> language : </span> albanian,english,german</p>
+               
+            </div>
+            <div class="box">
+                <p> <span> phone : </span> 044-814-280 </p>
+                <p> <span> email : </span> bardha.qoqaj123@gmail.com </p>
+                <p> <span> country : </span> Kosova </p>
+            </div>
+        </div>
+        <a href="#" class="btn">download CV</a>
+        <a href="#" class="btn">hire me</a>
+    </div>
+
+</div>
+
+<h1 class="heading"> <span> My </span> skills </h1>
+
+<div class="row-2">
+
+    <div class="skills">
+        <div class="progress">
+            <h3> web design <span> 95% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> web development <span> 80% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> UI design <span> 84% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> Database Management <span> 85% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> Project management  <span> 85% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> Microsoft Office Suite and Google <span> 95% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+    </div>
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3> >> 2+ </h3>
+            <p>years in computer science</p>
+        </div>
+       
+        <div class="box">
+            <h3> >> 6+ </h3>
+            <p>projects completed</p>
+        </div>
+        <div class="box">
+            <h3> >> 5 </h3>
+            <p>certifications earned</p>
+        </div>
+
+    </div>
+
+</div>
+
+</section>
+
+
 
 
 
